a14382979651c0ac75fb3a22124f007e78782862
kirillkeril <89710374+kirillkeril@users.noreply.github.com>
2023-09-30 11:56:20 +0500

    first commit

---
+++ bin/broduaga.dart
@@ -0,0 +1,5 @@
+import 'package:broduaga/broduaga.dart' as broduaga;
+
+void main(List<String> arguments) {
+  print('Hello world: ${broduaga.calculate()}!');
+}
---
+++ test/broduaga_test.dart
@@ -0,0 +1,8 @@
+import 'package:broduaga/broduaga.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
